b3f75cc88371884b010ab065b838e8df7b6bd61c
Wonjin Kim <wonjinism@gmail.com>
2021-01-10 20:10:39 +0900

    apollo graphql

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloProvider} from '@apollo/react-hooks'
+import client from './apollo'
 
 ReactDOM.render(
+  <ApolloProvider client={client}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,90 @@
-export default () => "Detail"
+import React from 'react'
+import { gql } from 'apollo-boost'
+import { useParams } from 'react-router-dom'
+import { useQuery } from "@apollo/react-hooks"
+import styled from "styled-components";
+
+
+const GET_MOVIE = gql`
+    query getMovie($id: Int!) {
+        movie(id: $id) {
+            title
+            medium_cover_image
+            language
+            rating
+            description_intro
+        }
+        suggestions(id: $id) {
+            id
+            medium_cover_image
+        }
+    }
+`
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 10px;
+  width: 50%;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${props => props.bg});
+  background-size: cover;
+  background-position: center center;
+`;
+
+export default () => {
+    // parameter 취득
+    const { id } = useParams()
+    console.log(id)
+    const { loading, data } = useQuery(GET_MOVIE, {
+        variables: { id: Number(id) }
+    })
+    console.log(data);
+    return (
+        <Container>
+            <Column>
+                <Title>
+                    {loading
+                        ? "Loading..."
+                        : `${data.movie.title} ${data.movie.isLiked ? "💖" : "😞"}`}
+                </Title>
+                {!loading && data.movie && (
+                    <>
+                        <Subtitle>{data.movie.language} · {data.movie.rating}
+                        </Subtitle>
+                        <Description>{data.movie.description_intro}</Description>
+                    </>
+                )}
+            </Column>
+            <Poster bg={data?.movie?.medium_cover_image}></Poster>
+        </Container>
+    )
+}
